ec90e4e6085e32ad460842a0f6028269237c0a32
choudharypooja <pooja.choudhary@logicmonitor.com>
2024-02-02 15:18:47 +0530

    more files

--- MyTimerTrigger1/__init__.py
+++ MyTimerTrigger1/__init__.py
@@ -2,7 +2,7 @@ import datetime
 import logging
 
 import azure.functions as func
-
+import okta_log_collector
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -11,4 +11,8 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
+    try:
+        okta_log_collector.collect_logs()
+    except Exception as e:
+                logging.error("error %s", str(e))
     logging.info('Python timer trigger function after changes ran at %s', utc_timestamp)
